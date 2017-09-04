20dc4ad304d7ab531e7d4343324d5a7030c2268c
qiujieqiong <qiujieqiong@linuxdeepin.com>
2017-09-04 14:15:23 +0800

    fix dir path err

--- docker_check/check_install_remove.sh
+++ docker_check/check_install_remove.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install python3-pip dbus
+apt-get -y install python3-pip systemd
 LC_CTYPE="en_US.UTF-8" pip3 install --trusted-host pypi.douban.com -i http://pypi.douban.com/simple/ pandas
-cd /docker_check_pkgs_install_remove/check_pkgs_install_remove/docker_check
+cd /docker_check_pkgs_install_remove/test_opem_app/docker_check
 #python3 get_sourcelist.py
 #mv /etc/apt/sources.list /etc/apt/sources.list.bak
 #mv base.list /etc/apt/sources.list.d/
